e7c3c7131df387a6199f6d2cfd6f43b63252ed36
fanghong <714067539@qq.com>
2019-03-07 19:23:59 +0800

    定义基本的数据返回结构

--- src/main/java/com/fanghong/pos/Result/JsonResult.java
+++ src/main/java/com/fanghong/pos/Result/JsonResult.java
@@ -6,9 +6,12 @@ import lombok.Data;
 @Data
 public class JsonResult {
 
+    @ApiModelProperty(name="status", value = "状态值", notes = "返回状态")
     private String status = null;
 
     @ApiModelProperty(name = "message", value = "状态信息", notes = "返回信息的内容")
     private String message = "";
-    private Object result = null;
+
+    @ApiModelProperty(name = "data", value = "数据", notes = "返回的数据结果对象")
+    private Object data = null;
 }
