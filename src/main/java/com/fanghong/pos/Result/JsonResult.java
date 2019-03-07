feb91e6b529c7c9feaa9127035f4929f37d81a27
fangh <714067539@qq.com>
2019-03-07 18:30:24 +0800

    调整API 返回对象信息

--- src/main/java/com/fanghong/pos/Result/JsonResult.java
+++ src/main/java/com/fanghong/pos/Result/JsonResult.java
@@ -1,9 +1,14 @@
 package com.fanghong.pos.Result;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
 public class JsonResult {
+
     private String status = null;
+
+    @ApiModelProperty(name = "message", value = "状态信息", notes = "返回信息的内容")
+    private String message = "";
     private Object result = null;
 }
