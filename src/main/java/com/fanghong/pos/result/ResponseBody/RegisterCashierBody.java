eb3a0aabb00d73e529bf48ec19f93f5ed47b240f
fanghong <714067539@qq.com>
2019-03-18 14:29:16 +0800

    result

--- src/main/java/com/fanghong/pos/result/ResponseBody/RegisterCashierBody.java
+++ src/main/java/com/fanghong/pos/result/ResponseBody/RegisterCashierBody.java
@@ -3,6 +3,8 @@ package com.fanghong.pos.result.ResponseBody;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
 public class RegisterCashierBody {
 
@@ -11,4 +13,7 @@ public class RegisterCashierBody {
 
     @ApiModelProperty(name = "cashierStatus", value = "收银员状态", notes = "默认1 启用")
     private String cashierStatus;
+
+    @ApiModelProperty(name = "cashierDiscountLevel", value = "默认折扣权限", notes = "0~1范围区间折扣")
+    private BigDecimal cashierDiscountLevel;
 }
